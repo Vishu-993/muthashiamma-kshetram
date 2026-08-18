1b8828952a3567d48cc3e604317acca829eabd06
Venom <116909259+Vishu-993@users.noreply.github.com>
2026-08-18 12:01:57 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,3 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 MONGO_URI = os.environ["MONGO_URI"]
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.get_default_database()
+
+pages_col = db["pages"]
+poojas_col = db["poojas"]
+activity_col = db["activity_log"]
